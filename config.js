028eb9910084e82179113333b918733979e48d2f
zistks <zist0207@gmail.com>
2026-02-11 08:39:10 +0900

    Update config values for Eikyuu Shounen

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    localStoragePrefix: "party-rank-sorter",
-    title: "Party Rank Sorter",
-    description: "Party rank sorter for your custom list of songs."
+    localStoragePrefix: "Eikyuu Shounen: Eternal Boys",
+    title: "Eikyuu Shounen: Eternal Boys",
+    description: "Eikyuu Shounen: Eternal Boys"
 };
